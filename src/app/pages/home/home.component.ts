296b69d1f31f2833147a9f841072430233098bc9
Romanz16 <zhyshkovych16@gmail.com>
2021-06-11 23:51:48 +0300

    slider for Novisign

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Playlist } from 'src/app/shared/classes/playlist.model';
-
+declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,7 +9,6 @@ import { Playlist } from 'src/app/shared/classes/playlist.model';
 export class HomeComponent implements OnInit {
   playlist: any =
     '{"screenKey":"0f127773-529f-4ff8-b211-af9e5c22a5bc","breakpointInterval":0,"playlists":[{"channelTime":0,"playlistItems":[{"creativeRefKey":null,"duration":10,"dataSize":586760,"expireDate":"9999-12-30 00:00:00","startDate":"0000-00-00 00:00:00","modified":null,"contentModified":null,"startReccurenceTime":null,"expireReccurenceTime":null,"selectedReccurenceDays":null,"collectStatistics":false,"creativeProperties":null,"creativeLabel":"pixabayImage-1518843.jpg","ignoreEventsWhilePlaying":false,"playOnlyOnEvent":false,"slidePriority":0,"playlistKey":"22abeab2-9905-452c-b9a6-73d444209a7e","creativeKey":"b1f1b49b-46b8-49ef-8177-309d28128bf7.jpg","orderKey":0,"eventTypesList":[]},{"creativeRefKey":null,"duration":19,"dataSize":7848203,"expireDate":"9999-12-30 00:00:00","startDate":"0000-00-00 00:00:00","modified":null,"contentModified":null,"startReccurenceTime":null,"expireReccurenceTime":null,"selectedReccurenceDays":null,"collectStatistics":false,"creativeProperties":null,"creativeLabel":"WhatsApp Video 2020-04-20 at 7.44....mp4","ignoreEventsWhilePlaying":false,"playOnlyOnEvent":false,"slidePriority":0,"playlistKey":"22abeab2-9905-452c-b9a6-73d444209a7e","creativeKey":"fbc403c5-e00f-4b35-9c8d-3c83217d3cbd.mp4","orderKey":1,"eventTypesList":[]},{"creativeRefKey":null,"duration":10,"dataSize":443634,"expireDate":"9999-12-30 00:00:00","startDate":"0000-00-00 00:00:00","modified":null,"contentModified":null,"startReccurenceTime":null,"expireReccurenceTime":null,"selectedReccurenceDays":null,"collectStatistics":false,"creativeProperties":null,"creativeLabel":"pixabayImage-5098479.jpg","ignoreEventsWhilePlaying":false,"playOnlyOnEvent":false,"slidePriority":0,"playlistKey":"22abeab2-9905-452c-b9a6-73d444209a7e","creativeKey":"df1b9a4b-f5ea-41f0-b778-3aaa1fc19906.jpg","orderKey":2,"eventTypesList":[]},{"creativeRefKey":null,"duration":30,"dataSize":6752046,"expireDate":"9999-12-30 00:00:00","startDate":"0000-00-00 00:00:00","modified":null,"contentModified":null,"startReccurenceTime":null,"expireReccurenceTime":null,"selectedReccurenceDays":null,"collectStatistics":false,"creativeProperties":null,"creativeLabel":"pixabayVideo-small-56493.mp4","ignoreEventsWhilePlaying":false,"playOnlyOnEvent":false,"slidePriority":0,"playlistKey":"22abeab2-9905-452c-b9a6-73d444209a7e","creativeKey":"86619515-6aef-4d0e-b2ec-da192c28d38d.mp4","orderKey":3,"eventTypesList":[]}],"playlistKey":"22abeab2-9905-452c-b9a6-73d444209a7e"}],"modified":1620115851282}';
-  //sliderIndex: number = 0;
 
   sliderList: Array<Playlist> = JSON.parse(this.playlist).playlists[0].playlistItems.map((elem: any) => {
     let type = elem.creativeKey.substring(elem.creativeKey.indexOf('.') + 1);
@@ -18,8 +17,6 @@ export class HomeComponent implements OnInit {
 
   public startSlider(num: number): void {
     let count = this.sliderList.length;
-
-    console.log(this.sliderList[num].duration);
     let duration = Number.parseInt(this.sliderList[num].duration) * 1000;
 
     switch (num) {
